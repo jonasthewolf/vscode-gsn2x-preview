f243d90882018491c584e7671b1a23e6b64ec86b
Jonas Wolf <jonas.wolf@jonaswolf.org>
2026-04-18 23:11:31 +0200

    Listens to editor changes.

--- src/extension.ts
+++ src/extension.ts
@@ -7,74 +7,103 @@ import * as os from 'os';
 import * as cp from 'child_process';
 import { copyYamlDependenciesRecursively } from './dependencyCopy';
 
-
 let svgPreviewPanel: vscode.WebviewPanel | undefined;
-let yamlFilePath:string | undefined;
+let yamlFilePath: string | undefined;
 let wordWrap: number | undefined;
 let lastSvgContent: string | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
-    const config = vscode.workspace.getConfiguration('gsn2xPreview');
-    const defaultPath = process.platform === 'win32' ? 'gsn2x.exe' : 'gsn2x';
-    const path_gsn2x = config.get<string>('gsn2xPath') ?? defaultPath;
-    wordWrap = config.get<number>('wordWrap');
+  const config = vscode.workspace.getConfiguration('gsn2xPreview');
+  const defaultPath = process.platform === 'win32' ? 'gsn2x.exe' : 'gsn2x';
+  const path_gsn2x = config.get<string>('gsn2xPath') ?? defaultPath;
+  wordWrap = config.get<number>('wordWrap');
 
-    // Create a temporary directory for SVG files
-    let tempDir = fs.mkdtempSync(path.join(os.tmpdir(), "gsn2x"));
+  // Create a temporary directory for SVG files
+  let tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'gsn2x'));
+  let yamlWatcher: vscode.FileSystemWatcher | undefined;
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('gsn2xPreview.open', async () => {
-            const activeEditor = vscode.window.activeTextEditor;
-            if (!activeEditor) {
-                vscode.window.showErrorMessage('No active editor.');
-                return;
-            }
+  const isYamlFile = (filePath: string) => /\.(ya?ml)$/i.test(filePath);
 
-			yamlFilePath = activeEditor.document.uri.fsPath;
-            // Set up a file watcher to update the preview on changes
-			const yamlWatcher = vscode.workspace.createFileSystemWatcher(yamlFilePath);
-			yamlWatcher.onDidChange(async (e) => {
-				let svg = await run_gsn2x(path_gsn2x, tempDir, e.fsPath);
-				showSvgPreview(svg);
-			});
+  const watchYamlFile = (filePath: string) => {
+    yamlWatcher?.dispose();
+    yamlWatcher = vscode.workspace.createFileSystemWatcher(filePath);
+    yamlWatcher.onDidChange(async (e) => {
+      if (e.fsPath === filePath) {
+        const svg = await run_gsn2x(path_gsn2x, tempDir, filePath);
+        showSvgPreview(svg);
+      }
+    });
+    context.subscriptions.push(yamlWatcher);
+  };
 
-            try {
-                let svg = await run_gsn2x(path_gsn2x, tempDir, yamlFilePath);
-				showSvgPreview(svg);
-            } catch (error) {
-                console.error(error);
-            }
-        })
-    );
+  const renderYamlEditor = async (editor: vscode.TextEditor | undefined) => {
+    if (!editor) {
+      return;
+    }
 
-    // Listen for theme changes and update the webview
-    context.subscriptions.push(
-        vscode.window.onDidChangeActiveColorTheme(() => {
-            if (svgPreviewPanel && lastSvgContent) {
-                const html = generateHtmlFromSvg(lastSvgContent);
-                svgPreviewPanel.webview.html = html;
-            }
-        })
-    );
+    const filePath = editor.document.uri.fsPath;
+    if (!isYamlFile(filePath)) {
+      return;
+    }
 
+    yamlFilePath = filePath;
+    watchYamlFile(filePath);
+
+    try {
+      const svg = await run_gsn2x(path_gsn2x, tempDir, filePath);
+      showSvgPreview(svg);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('gsn2xPreview.open', async () => {
+      await renderYamlEditor(vscode.window.activeTextEditor);
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(async (editor) => {
+      await renderYamlEditor(editor);
+    })
+  );
+
+  // Listen for theme changes and update the webview
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveColorTheme(() => {
+      if (svgPreviewPanel && lastSvgContent) {
+        const html = generateHtmlFromSvg(lastSvgContent);
+        svgPreviewPanel.webview.html = html;
+      }
+    })
+  );
+
+  // Render current active editor if it is YAML at startup
+  if (vscode.window.activeTextEditor) {
+    renderYamlEditor(vscode.window.activeTextEditor);
+  }
 }
 
-
-async function run_gsn2x(command: string, outputPath: string, yamlFilePath: string): Promise<string> {
-    // Execute the configured command-line tool
-    return new Promise<string>((resolve, reject) => {
-		const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
-		const yamlintemp = copyYamlDependenciesRecursively(yamlFilePath, outputPath, workspaceFolder);
-		const directory = path.dirname(yamlintemp);
-    	const baseName = path.basename(yamlintemp, path.extname(yamlintemp));
-    	const svgOutputPath = path.join(directory, `${baseName}.svg`);
-        let args = ['-E', yamlintemp, `-o=${outputPath}`];
-        if (wordWrap) {
-            args.push('-w=' + wordWrap.toString());
-        }
-        const childProcess = cp.execFile(command, args, (error, stdout, stderr) => {
-            if (error) {
-                resolve(`<html>
+async function run_gsn2x(
+  command: string,
+  outputPath: string,
+  yamlFilePath: string
+): Promise<string> {
+  // Execute the configured command-line tool
+  return new Promise<string>((resolve, reject) => {
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+    const yamlintemp = copyYamlDependenciesRecursively(yamlFilePath, outputPath, workspaceFolder);
+    const directory = path.dirname(yamlintemp);
+    const baseName = path.basename(yamlintemp, path.extname(yamlintemp));
+    const svgOutputPath = path.join(directory, `${baseName}.svg`);
+    let args = ['-E', yamlintemp, `-o=${outputPath}`];
+    if (wordWrap) {
+      args.push('-w=' + wordWrap.toString());
+    }
+    const childProcess = cp.execFile(command, args, (error, stdout, stderr) => {
+      if (error) {
+        resolve(`<html>
                             <head>
                                 <style>body { color: red; }</style>
                             </head>
@@ -84,25 +113,26 @@ async function run_gsn2x(command: string, outputPath: string, yamlFilePath: stri
                                 <pre>${stderr}</pre>
                             </body>
                         </html>`);
-            } else {
-                const svgContent = fs.readFileSync(svgOutputPath, 'utf8');
-                resolve(svgContent);
-            }
-        });
-
-        // Capture standard error output
-        childProcess.stderr?.on('data', (data) => {
-            console.error(data.toString());
-        });
+      } else {
+        const svgContent = fs.readFileSync(svgOutputPath, 'utf8');
+        resolve(svgContent);
+      }
     });
+
+    // Capture standard error output
+    childProcess.stderr?.on('data', (data) => {
+      console.error(data.toString());
+    });
+  });
 }
 
 function generateHtmlFromSvg(svgContent: string): string {
-    const theme = vscode.window.activeColorTheme;
-    const isDark = theme.kind === vscode.ColorThemeKind.Dark || theme.kind === vscode.ColorThemeKind.HighContrast;
-    const svgFilter = isDark ? 'filter: invert(1);' : '';
+  const theme = vscode.window.activeColorTheme;
+  const isDark =
+    theme.kind === vscode.ColorThemeKind.Dark || theme.kind === vscode.ColorThemeKind.HighContrast;
+  const svgFilter = isDark ? 'filter: invert(1);' : '';
 
-    return `<html>
+  return `<html>
                 <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=20, user-scalable=yes">
@@ -270,32 +300,31 @@ function generateHtmlFromSvg(svgContent: string): string {
 }
 
 function showSvgPreview(svg: string) {
-    lastSvgContent = svg;
-    const html = generateHtmlFromSvg(svg);
-    if (svgPreviewPanel) {
-        // Update existing preview
-        svgPreviewPanel.webview.html = html;
-    } else {
-        // Create a new preview panel
-        svgPreviewPanel = vscode.window.createWebviewPanel(
-            'gsnPreview',
-            'GSN Preview',
-            vscode.ViewColumn.Beside,
-            {
-                enableScripts: true
-            }
-        );
-        svgPreviewPanel.webview.html = html;
-        
+  lastSvgContent = svg;
+  const html = generateHtmlFromSvg(svg);
+  if (svgPreviewPanel) {
+    // Update existing preview
+    svgPreviewPanel.webview.html = html;
+  } else {
+    // Create a new preview panel
+    svgPreviewPanel = vscode.window.createWebviewPanel(
+      'gsnPreview',
+      'GSN Preview',
+      vscode.ViewColumn.Beside,
+      {
+        enableScripts: true,
+      }
+    );
+    svgPreviewPanel.webview.html = html;
 
-        // Dispose the panel when closed
-        svgPreviewPanel.onDidDispose(() => {
-            svgPreviewPanel = undefined;
-        });
-    }
+    // Dispose the panel when closed
+    svgPreviewPanel.onDidDispose(() => {
+      svgPreviewPanel = undefined;
+    });
+  }
 }
 
 export function deactivate() {
-    // Clean up resources when the extension is deactivated
-    // Remove the temporary directory if needed
+  // Clean up resources when the extension is deactivated
+  // Remove the temporary directory if needed
 }
